bf36389e18b61ed3bed7d8020c4c35372f31e23d
14110145 <waptop4u@gmail.com>
2020-03-16 15:33:28 +0700

    Luyen tap

---
+++ db.js
@@ -0,0 +1,10 @@
+var low = require('lowdb');
+var Filesync = require('lowdb/adapters/FileSync');
+var adapter = new Filesync('db.json');
+db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: []})
+  .write()
+
+  module.exports = db;
