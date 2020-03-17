d38dca556676eab29528f86f0a8ea0fd7e66b679
14110145 <waptop4@gmail.com>
2020-03-17 16:59:28 +0700

    3. Add basic layout

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'))
 
 app.get('/', function(req , res){
     res.render('index.pug',{
