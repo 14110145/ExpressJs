086a665bc095faeb80e238a09a28c29d1cb74faa
14110145 <waptop4u@gmail.com>
2020-03-18 16:37:29 +0700

    7. Login form -  Cookie

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var shortid = require('shortid');
+var authMiddleware = require('./middlewares/auth.middleware');
+
 
 var userRoute = require('./routes/user.route');
+var authRoute = require('./routes/auth.route');
+
 var db = require('./db');
 
 var port = 3000;
@@ -12,14 +17,18 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use('/auth', authRoute);
+app.use('/users', authMiddleware.requireAuth , userRoute);
+
 
 app.get('/', function(req , res){
     res.render('index.pug',{
         name: 'Lam Phu'
     });
 });
-app.use('/users', userRoute);
 
 app.listen(port, function(){
     console.log('Example app listening on port' + port);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var controller = require('../controllers/user.controller');
 var router = express.Router();
-var validate = require('../validate/user.validate')
+var validate = require('../validate/user.validate');
+var authMiddleware = require('../middlewares/auth.middleware');
+
+
 
 router.get('/', controller.index);
+router.get('/cookie', function(req, res){
+    res.cookie('user-id', 12345);
+    res.send('Hello');
+});
+
 router.get('/search', controller.search);
 router.get('/create', controller.create);
 router.get('/:id', controller.getId);
