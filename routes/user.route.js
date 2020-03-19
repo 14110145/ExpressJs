886f3dbf05cb465d1049bc20b0e777c8eb192a11
14110145 <waptop4u@gmail.com>
2020-03-19 22:23:13 +0700

    13. File upload- multer

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/user.controller');
 var validate = require('../validate/user.validate');
-// var authMiddleware = require('../middlewares/auth.middleware');
+var multer = require('multer');
 
-
+var upload = multer({dest: './public/uploads/'});
 
 router.get('/', controller.index);
 // router.get('/cookie', function(req, res){
@@ -14,7 +14,8 @@ router.get('/', controller.index);
 router.get('/search', controller.search);
 router.get('/create', controller.create);
 router.get('/:id', controller.getId);
-router.post('/create', validate.postCreate ,controller.postCreate);
+router.post('/create', 
+    upload.single('avatar'), validate.postCreate ,controller.postCreate);
 
 
 module.exports = router;
